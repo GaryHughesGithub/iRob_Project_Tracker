9380317681303368a52c005bad41dec2c5be7ea7
stefand2003 <119484929+stefand2003@users.noreply.github.com>
2023-06-27 06:11:12 +0100

    React-Router-Dom for App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.scss';
 
 import Authentication from './components/Auth/Authentication';
-import Dashboard from './components/Dashboard/Dashboard';
-import EmailCode from './components/ForgotPass/EmailCode/EmailCode';
-import NewPassword from './components/ForgotPass/NewPassword/NewPassword';
-import ResetPass from './components/ForgotPass/ResetPass';
-import Menu from './components/Menu/Menu';
-import PostRegister from './components/Register/PostRegister/PostRegister';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import EmailCode from './components/ForgotPass/EmailCode/EmailCode';
+// import NewPassword from './components/ForgotPass/NewPassword/NewPassword';
+// import ResetPass from './components/ForgotPass/ResetPass';
+// import Menu from './components/Menu/Menu';
+// import PostRegister from './components/Register/PostRegister/PostRegister';
 import Register from './components/Register/Register';
+import Home from './pages/Home/Home';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ display: 'flex', gap: '10px' }}>
-      <Menu />
-      <Dashboard />
+    <div>
+      <Routes>
+        <Route path='/login' element={<Authentication />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Home />} />
+      </Routes>
     </div>
   );
 }
