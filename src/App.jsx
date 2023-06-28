acf815aa0d1538984f91f1171e38fd55ddac284c
Kingsley Ijomah <git-example@codehance>
2023-06-28 09:04:53 -0300

    Adding dashboard as home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import './App.scss';
 
 import Authentication from './components/Auth/Authentication';
-// import Dashboard from './components/Dashboard/Dashboard';
-// import EmailCode from './components/ForgotPass/EmailCode/EmailCode';
-// import NewPassword from './components/ForgotPass/NewPassword/NewPassword';
-// import ResetPass from './components/ForgotPass/ResetPass';
-// import Menu from './components/Menu/Menu';
-// import PostRegister from './components/Register/PostRegister/PostRegister';
 import Register from './components/Register/Register';
 import Home from './pages/Home/Home';
 
@@ -16,9 +10,9 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/login' element={<Authentication />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Authentication />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
