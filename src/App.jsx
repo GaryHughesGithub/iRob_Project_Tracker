ce304cb1ce18c2e8b9899800cf460004487efed0
stefand2003 <119484929+stefand2003@users.noreply.github.com>
2023-07-13 16:36:02 +0300

    Added the PLC table

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Register from './components/Register/Register';
 import Home from './pages/Home/Home';
 
 import { Route, Routes } from 'react-router-dom';
+import MyPLC from './pages/MyPLC/MyPLC';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Authentication />} />
-        <Route path="/register" element={<Register />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/myPLC' element={<MyPLC />} />
+        <Route path='/login' element={<Authentication />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </div>
   );
