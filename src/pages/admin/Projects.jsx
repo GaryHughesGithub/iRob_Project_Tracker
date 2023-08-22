813235dc50d2c241f3c02295a3ddf9bce559b20d
stefand2003 <119484929+stefand2003@users.noreply.github.com>
2023-08-22 14:58:43 +0100

    applied modal to projects

--- src/pages/admin/Projects.jsx
+++ src/pages/admin/Projects.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import Layout from '../../components/Admin/Layout';
 import { Link } from 'react-router-dom';
+import Modal from '../../components/Modal/Modal';
 
-const zones = [
+const projects = [
   { name: 'JLR' },
   {
     name: 'Vauxhall',
@@ -13,8 +14,107 @@ const zones = [
 ];
 
 export default function Projects() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [currentproject, setCurrentproject] = useState(null);
+
+  const handleOpenModal = (project) => {
+    if (project) {
+      setName(project.name);
+      setStatus(project.status);
+      setCurrentproject(project);
+    } else {
+      setName('');
+      setStatus('');
+      setCurrentproject(null);
+    }
+    setModalOpen(true);
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+
+    setName('');
+    setStatus('');
+    setCurrentproject(null);
+    setModalOpen(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (currentproject) {
+      // handle update here
+    } else {
+      // handle create here
+    }
+    setModalOpen(false);
+  };
   return (
     <Layout>
+      <Modal
+        title={currentproject ? 'Edit project' : 'Create New project'}
+        modalOpen={modalOpen}
+        handleModalClose={() => setModalOpen(false)}
+      >
+        <div className='w-[400px] sm:w-[650px]'>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-4'>
+              <label
+                htmlFor='name'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Name
+              </label>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className='mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md'
+              />
+            </div>
+
+            <div className='mb-4'>
+              <label
+                htmlFor='status'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Status
+              </label>
+              <select
+                id='status'
+                name='status'
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+              >
+                <option>Active</option>
+                <option>Pending</option>
+                <option>Disabled</option>
+              </select>
+            </div>
+
+            <div className='flex justify-end'>
+              <button
+                type='button'
+                onClick={handleClose}
+                className='mr-2 inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-700'
+              >
+                Cancel
+              </button>
+              <button
+                type='submit'
+                className='inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700'
+              >
+                {currentproject ? 'Save Changes' : 'Create'}
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+
       <div className='px-4 sm:px-6 lg:px-8'>
         <div className='sm:flex sm:items-center'>
           <div className='sm:flex-auto'>
@@ -29,6 +129,7 @@ export default function Projects() {
             <button
               type='button'
               className='block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              onClick={() => handleOpenModal()}
             >
               Add New Project
             </button>
@@ -62,27 +163,28 @@ export default function Projects() {
                   </tr>
                 </thead>
                 <tbody className='bg-white'>
-                  {zones.map((zone) => (
-                    <tr key={zone.name} className='even:bg-gray-50'>
+                  {projects.map((project) => (
+                    <tr key={project.name} className='even:bg-gray-50'>
                       <td className='whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3'>
-                        {zone.name}
+                        {project.name}
                       </td>
 
                       <td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3'>
                         <button className='text-indigo-600 hover:text-indigo-900'>
-                          Edit<span className='sr-only'>, {zone.name}</span>
+                          Edit<span className='sr-only'>, {project.name}</span>
                         </button>
 
                         <button className='text-indigo-600 hover:text-indigo-900 ml-6'>
-                          Delete<span className='sr-only'>, {zone.name}</span>
+                          Delete
+                          <span className='sr-only'>, {project.name}</span>
                         </button>
 
                         <Link
-                          to='/admin/zone-tasks'
+                          to='/admin/project-tasks'
                           className='text-indigo-600 hover:text-indigo-900 ml-5'
                         >
-                          View Zones
-                          <span className='sr-only'>, {zone.name}</span>
+                          View projects
+                          <span className='sr-only'>, {project.name}</span>
                         </Link>
                       </td>
                     </tr>
